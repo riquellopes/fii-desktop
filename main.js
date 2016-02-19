55b5a9ca0a3173b276c9c1571f07e2f6719096de
Henrique Lopes <riquellopes@gmail.com>
2016-02-19 20:34:31 -0200

    removendo dev tool

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ app.on("window-all-closed", function(){
 app.on("ready", function(){
     mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadURL("file://" + __dirname + "/index.html");
-    mainWindow.webContents.openDevTools();
 
 
     mainWindow.on("closed", function(){
